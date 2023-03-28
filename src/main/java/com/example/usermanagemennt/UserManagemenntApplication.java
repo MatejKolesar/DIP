5c22c2dd42da7003ebd10311e63f53e81a3649ca
Matej Kolesar <xkoles07@stud.fit.vutbr.cz>
2023-03-28 15:13:56 +0200

    first commit

---
+++ src/main/java/com/example/usermanagemennt/UserManagemenntApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermanagemennt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagemenntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagemenntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usermanagemennt/UserManagemenntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermanagemennt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagemenntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
